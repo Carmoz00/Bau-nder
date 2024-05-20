dcbf0511d5457f38482bdf7bfda5205e10cbd895
Alessandra Lo Coco <lococoale@gmail.com>
2024-05-20 10:52:38 +0200

    Ciao

---
+++ models/User.js
@@ -0,0 +1,8 @@
+class ModelUser {
+  constructor(username, password, email) {
+    this.id_user = Math.random();
+    this.username = username;
+    this.password = password;
+    this.email = email;
+  }
+}
