f23220b95ada65ccce3e6fd4f36dc1f8fdc9fce2
AlessandroDiBenedetto <alessandrodibenedetto29@gmail.com>
2024-06-20 23:09:58 +0200

    feat/aggiunta pagine di profiloUser

--- pages/profiloUser/scripts.js
+++ pages/profiloUser/scripts.js
@@ -3,9 +3,22 @@ const isLogged = !!localStorage.getItem("user");
 if (!isLogged) window.location.href = "../login";
 else {
   const homePageButton = document.getElementById("logo");
+
+  const modificaProfiloButton = document.getElementById(
+    "modificaProfiloButton"
+  );
   const listaCaniPersonaleButton = document.getElementById(
     "listaCaniPersonaleButton"
   );
+
+  const RichiesteInviateButton = document.getElementById(
+    "RichiesteInviateButton"
+  );
+  const richiesteRicevuteButton = document.getElementById(
+    "richiesteRicevuteButton"
+  );
+
+  const listaMatchButton = document.getElementById("listaMatchButton");
 
   homePageButton.addEventListener("click", () => {
     window.location.href = "../homePage/index.html";
@@ -13,5 +26,21 @@ else {
 
   listaCaniPersonaleButton.addEventListener("click", () => {
     window.location.href = "../listaCani/index.html";
+  });
+
+  modificaProfiloButton.addEventListener("click", () => {
+    window.location.href = "../modificaProfilo/index.html";
+  });
+
+  listaMatchButton.addEventListener("click", () => {
+    window.location.href = "../myMatches/index.html";
+  });
+
+  RichiesteInviateButton.addEventListener("click", () => {
+    window.location.href = "../richiesteInviate/index.html";
+  });
+
+  richiesteRicevuteButton.addEventListener("click", () => {
+    window.location.href = "../richiesteRicevute/index.html";
   });
 }
